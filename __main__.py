099b8d368e718c036d0f00f826f3396598bfedca
nw <nico.walters@techtalents.es>
2020-06-13 18:06:44 +0200

    Base64 encoding

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import base64
 
 load_dotenv()
 
@@ -8,4 +9,12 @@ secret = os.getenv("secret")
 
 def authenticate(client_id):
     endpoint = "https://accounts.spotify.com/api/token"
+
+    # All the weird stuff Spotify is asking us to do
+    id_secret = client_id + ":" + secret
+    encoded = id_secret.encode("utf-8")
+    converted = base64.b64encode(encoded)
+    decoded = converted.decode("utf-8")
+
+    print(decoded)
 
