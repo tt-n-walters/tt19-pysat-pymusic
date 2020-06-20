f354ab6b378fba01d2188a50dd50623e9a6faded
nw <nico.walters@techtalents.es>
2020-06-20 17:26:59 +0200

    Refactored authenticate methods

--- __main__.py
+++ __main__.py
@@ -4,45 +4,11 @@ import base64
 import requests
 import time
 
+from authenticate import tokens
 
 load_dotenv()
 
 secret = os.getenv("secret")
-
-
-def authenticate(client_id):
-    endpoint = "https://accounts.spotify.com/api/token"
-
-    # All the weird stuff Spotify is asking us to do
-    id_secret = client_id + ":" + secret
-    encoded = id_secret.encode("utf-8")
-    converted = base64.b64encode(encoded)
-    decoded = converted.decode("utf-8")
-
-    data = {
-        "grant_type": "client_credentials"
-    }
-    headers = {
-        "Authorization": "Basic " + decoded
-    }
-    r = requests.post(endpoint, data=data, headers=headers)
-    json = r.json()
-
-    current_time = time.time()
-    valid_until = current_time + json["expires_in"]
-
-    return json["access_token"], valid_until
-
-
-def token_generator(client_id):
-    token = None
-    valid_until = 0
-
-    while True:
-        # Check if expired
-        if time.time() > valid_until:
-            token, valid_until = authenticate(client_id)
-        yield token
 
 
 def searcher(tokens, search_type):
@@ -61,7 +27,6 @@ def searcher(tokens, search_type):
     return search
 
 
-tokens = token_generator("643f62e46f4f46b9aeb17d4d63929497")
 
 search_artist = searcher(tokens, "artist")
 search_track = searcher(tokens, "track")
---
+++ authenticate.py
@@ -0,0 +1,37 @@
+
+def authenticate(client_id):
+    endpoint = "https://accounts.spotify.com/api/token"
+
+    # All the weird stuff Spotify is asking us to do
+    id_secret = client_id + ":" + secret
+    encoded = id_secret.encode("utf-8")
+    converted = base64.b64encode(encoded)
+    decoded = converted.decode("utf-8")
+
+    data = {
+        "grant_type": "client_credentials"
+    }
+    headers = {
+        "Authorization": "Basic " + decoded
+    }
+    r = requests.post(endpoint, data=data, headers=headers)
+    json = r.json()
+
+    current_time = time.time()
+    valid_until = current_time + json["expires_in"]
+
+    return json["access_token"], valid_until
+
+
+def token_generator(client_id):
+    token = None
+    valid_until = 0
+
+    while True:
+        # Check if expired
+        if time.time() > valid_until:
+            token, valid_until = authenticate(client_id)
+        yield token
+
+
+tokens = token_generator("643f62e46f4f46b9aeb17d4d63929497")
