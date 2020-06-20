d5b69a0acc0d8e907d1aa5d5aacd13e028afa4e8
nw <nico.walters@techtalents.es>
2020-06-20 18:48:29 +0200

    Progress

--- __main__.py
+++ __main__.py
@@ -53,8 +53,24 @@ def get_recommendations(token, artists=[], tracks=[]):
         "Authorization": "Bearer " + token
     }
     r = requests.get(endpoint, params=parameters, headers=headers)
-    
+
+    response = r.json()[search_type + "s"]["items"]
+    data = []
+    for result in response:
+        item = {
+            "link": result["external_urls"]["spotify"],
+            "name": result["name"],
+            "id": result["id"]
+        }
+        if search_type == "track":
+            item["artist_name"] = result["artists"][0]["name"]
+
+        data.append(item)
+
+    return data
 
 
     
-search_track("Wish you were here")
+# search_track("Wish you were here")
+
+get_recommendations(next(tokens), artists=["0k17h0D3J5VfsdmQ1iZtE9"])
